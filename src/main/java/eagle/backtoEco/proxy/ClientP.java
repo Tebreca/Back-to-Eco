fbe2c88694690bd75f347540b879374309aff183
Tebreca <ewan.arends10@gmail.com>
2017-05-04 09:59:37 +0200

    Mod
    I did Fix some setup errors and deleted the eaglcore

--- src/main/java/eagle/backtoEco/proxy/ClientP.java
+++ src/main/java/eagle/backtoEco/proxy/ClientP.java
@@ -1,11 +1,8 @@
 package eagle.backtoEco.proxy;
 
 import eagle.backtoEco.main.Vars;
-import eagleCore.main.Registry;
 import net.minecraft.item.Item;
 
 public class ClientP extends ServerP{
-	
-	
 	
 }
